ac9aeedf5e188d59169edf5763b79093b0d06bd0
Sean OToole <seanotoole@u.boisestate.edu>
2024-07-17 15:35:51 -0600

    wadi autogen tweaks, generation of cmdln output for end of simulation

--- examples/CPS_Functionality_Testing/wadi_autogen.py
+++ examples/CPS_Functionality_Testing/wadi_autogen.py
@@ -9,7 +9,7 @@ from wntr.network.layer import autogenerate_full_cps_layer
 import plotly.express as px
 import networkx as nx
 import numpy
-
+import pandas as pd
 import argparse
 from pyModbusTCP.server import ModbusServer, DataHandler
 from pyModbusTCP.constants import EXP_ILLEGAL_FUNCTION
@@ -23,4 +23,22 @@ import os
 # Create a water network model
 inp_file = '../networks/wadi_topology/wadi_map.inp'
 wn = wntr.network.WaterNetworkModel(inp_file)
-autogenerate_full_cps_layer(wn, placement_type='complex', timed_control_assignments='local', edge_types='MODBUS', verbose=1)
+autogenerate_full_cps_layer(wn, placement_type='complex', timed_control_assignments='local', edge_types='MODBUS', verbose=0)
+wn.options.time.duration = 86400
+# Need EPANET sim for water quality analysis
+sim = wntr.sim.WNTRSimulator(wn)
+results = sim.run_sim()  
+#df_node = pd.DataFrame([])
+for key in results.node:
+    #print(results.node[key].to_csv("wadi_out.csv",mode='a'))
+    print(key)
+    print(results.node[key].loc[86400,:].to_csv())
+    #pd.concat([df,results.node[key]],sort='false')
+#df_link = 
+for key in results.link:
+    print(key)
+    #print(results.link[key].to_csv("wadi_out.csv",mode='a'))
+    print(results.link[key].loc[86400,:].to_csv())
+    #pd.concat([df,results.link[key]],sort='false')    
+#df.to_csv("wadi_out.csv")
+#print(df)
